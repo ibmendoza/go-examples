c00fa7443047c0e4efc1d77cf382872237989f72
Isagani Mendoza <gani.mendoza@gmail.com>
2015-05-30 06:22:44 +0800

    Update set.go

--- set.go
+++ set.go
@@ -1,32 +1,48 @@
-//Courtesy: Fatih Arslan et al
+//courtesy: https://github.com/fatih/set
 
 package main
 
 import (
-    "fmt"
-    "github.com/fatih/set"
-    "strconv"
-    "sync"
+	"fmt"
+	"gopkg.in/fatih/set.v0"
 )
 
 func main() {
-    var wg sync.WaitGroup // this is just for waiting until all goroutines finish
+	// ... or with some initial values
+	s := set.New("istanbul", "frankfurt", 30.123, "san francisco", 1234)
+	// add items
+	s.Add("istanbul")
+	s.Add("istanbul") // nothing happens if you add duplicate item
 
-    // Initialize our thread safe Set
-    s := set.New()
+	// add multiple items
+	s.Add("ankara", "san francisco", 3.14)
 
-    // Add items concurrently (item1, item2, and so on)
-    for i := 0; i < 10; i++ {
-        wg.Add(1)
-        go func(i int) {
-            item := "item" + strconv.Itoa(i)
-            fmt.Println("adding", item)
-            s.Add(item)
-            wg.Done()
-        }(i)
-    }
-    
-    // Wait until all concurrent calls finished and print our set
-    wg.Wait()
-    fmt.Println(s)
+	// remove item
+	s.Remove("frankfurt")
+	s.Remove("frankfurt") // nothing happes if you remove a nonexisting item
+
+	// remove multiple items
+	s.Remove("barcelona", 3.14, "ankara")
+
+	// removes an arbitary item and return it
+	item := s.Pop()
+
+	fmt.Println(item)
+
+	// create a new copy
+	other := s.Copy()
+	fmt.Println(other)
+
+	// number of items in the set
+	len := s.Size()
+	fmt.Println(len)
+
+	// return a list of items
+	items := s.List()
+	fmt.Println(items)
+
+	// string representation of set
+	fmt.Printf("set is %s\n", s.String())
+
+	//fmt.Println([]byte(s.String()))
 }
