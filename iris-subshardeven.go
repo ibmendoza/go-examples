8852a9a5e6c994091f0e13950bfe7f63ee4152e6
Isagani Mendoza <gani.mendoza@gmail.com>
2015-09-17 11:04:42 +0800

    Create iris-subshardeven.go

---
+++ iris-subshardeven.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gopkg.in/project-iris/iris-go.v1"
+	"log"
+	"runtime"
+	"time"
+)
+
+type topicEvent struct{}
+type doneEvent struct{}
+
+var start = time.Now()
+var numbPtr = flag.Int("msg", 10000, "number of messages (default: 10000)")
+
+func (t topicEvent) HandleEvent(event []byte) {}
+
+func (t doneEvent) HandleEvent(event []byte) {
+	elapsed := time.Since(start)
+	log.Printf("Time took %s", elapsed)
+}
+
+func main() {
+
+	flag.Parse()
+
+	conn, err := iris.Connect(55555)
+	if err != nil {
+		log.Fatalf("failed to connect to the Iris relay: %v.", err)
+	} else {
+		log.Println("Connected to port 55555")
+	}
+
+	var topicHandler = new(topicEvent)
+	var doneHandler = new(doneEvent)
+
+	conn.Subscribe("even", topicHandler, &iris.TopicLimits{
+		EventThreads: runtime.NumCPU(),
+		EventMemory:  64 * 1024 * 1024,
+	})
+
+	conn.Subscribe("done", doneHandler, &iris.TopicLimits{
+		EventThreads: runtime.NumCPU(),
+		EventMemory:  64 * 1024 * 1024,
+	})
+
+	defer conn.Close()
+
+	fmt.Scanln()
+}
