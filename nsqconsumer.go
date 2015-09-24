1f6ea9a53b579090cdd6cea3d6b56eaad98f7779
Isagani Mendoza <gani.mendoza@gmail.com>
2015-09-24 19:20:44 +0800

    Create nsqconsumer.go

---
+++ nsqconsumer.go
@@ -0,0 +1,29 @@
+//http://tleyden.github.io/blog/2014/11/12/an-example-of-using-nsq-from-go/
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/nsqio/go-nsq"
+)
+
+func main() {
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	config := nsq.NewConfig()
+	q, _ := nsq.NewConsumer("write_test", "ch", config)
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Got a message: %v", message)
+		wg.Done()
+		return nil
+	}))
+	err := q.ConnectToNSQD("127.0.0.1:4150")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	wg.Wait()
+
+}
