79dcc6c1691a37cd1f22aeedece93d5e79c57304
Isagani Mendoza <gani.mendoza@gmail.com>
2015-10-08 19:33:11 +0800

    Create nsqpub2.go

---
+++ nsqpub2.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nsqio/go-nsq"
+	"log"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()1234567890")
+var numbPtr = flag.Int("msg", 10000, "number of messages (default: 10000)")
+var ipnsqd = flag.String("ipnsqd", "", "IP address of nsqd")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+	/*
+	   Below are the default port settings
+	   nsqd listens at port 4150 (for TCP clients), 4151 (for HTTP clients)
+
+	   nsqlookupd listens at port 4160 (for TCP clients), 4161 (for HTTP clients)
+
+	   nsqadmin listens at port 4171 (for HTTP clients) or
+	     to be specified (for go-nsq clients) q.ConnectToNSQLookupd("127.0.0.1:4161")
+
+	   http://tleyden.github.io/blog/2014/11/12/an-example-of-using-nsq-from-go/
+	   $ nsqlookupd &
+	   $ nsqd --lookupd-tcp-address=127.0.0.1:4160 &
+	   $ nsqadmin --lookupd-http-address=127.0.0.1:4161 &
+	*/
+
+	config := nsq.NewConfig()
+
+	//ipaddr, _ := lib.GetIPAddress()
+
+	//w, _ := nsq.NewProducer("127.0.0.1:4150", config)
+
+	w, _ := nsq.NewProducer(*ipnsqd+":4150", config)
+
+	err := w.Publish("write_test", []byte("test"))
+	if err != nil {
+		log.Fatal("Could not connect")
+	}
+
+	flag.Parse()
+
+	start := time.Now()
+
+	for i := 1; i <= *numbPtr; i++ {
+		w.Publish("test", []byte(randSeq(320)))
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Time took %s", elapsed)
+
+	w.Stop()
+
+	fmt.Scanln()
+}
