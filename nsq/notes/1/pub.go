edd13c75f3c5f18595ab23793dd38e72669417a8
Isagani Mendoza <gani.mendoza@gmail.com>
2015-12-01 17:25:30 +0800

    Create pub.go

---
+++ nsq/notes/1/pub.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ibmendoza/go-lib"
+	"github.com/nsqio/go-nsq"
+	"log"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()1234567890")
+var numbPtr = flag.Int("msg", 10000, "number of messages (default: 10000)")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+
+	config := nsq.NewConfig()
+
+	ipaddr, _ := lib.GetIPAddress()
+
+	w, err := nsq.NewProducer(ipaddr+":4150", config)
+
+	if err != nil {
+		log.Fatal("Could not connect")
+	}
+
+	flag.Parse()
+
+	start := time.Now()
+
+	for i := 1; i <= *numbPtr; i++ {
+		w.Publish("test", []byte(randSeq(320)))
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Time took %s", elapsed)
+
+	w.Stop()
+
+	fmt.Scanln()
+}
