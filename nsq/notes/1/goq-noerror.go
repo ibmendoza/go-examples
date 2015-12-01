ac946bb1dba834b4f681196a990cde390b7e9ab3
Isagani Mendoza <gani.mendoza@gmail.com>
2015-12-01 17:06:37 +0800

    Create goq-noerror.go

---
+++ nsq/notes/1/goq-noerror.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/itmarketplace/go-queue"
+	"github.com/nsqio/go-nsq"
+	"runtime"
+	"time"
+)
+
+var start = time.Now()
+var ops uint64 = 0
+var numbPtr = flag.Int("msg", 10000, "number of messages (default: 10000)")
+var lkp = flag.String("lkp", "", "IP address of nsqlookupd")
+
+func main() {
+
+	flag.Parse()
+
+	c := queue.NewConsumer("test", "ch")
+
+	c.Set("nsqlookupd", *lkp+":4161")
+	c.Set("concurrency", runtime.GOMAXPROCS(runtime.NumCPU()))
+	c.Set("max_attempts", 10)
+	c.Set("max_in_flight", 150)
+	c.Set("default_requeue_delay", "15s")
+
+	c.Start(nsq.HandlerFunc(func(msg *nsq.Message) error {
+		return nil
+	}))
+
+	fmt.Scanln()
+}
