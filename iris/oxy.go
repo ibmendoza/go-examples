fa7a8d05ea53b029064131f6ce964120e9ff530c
Isagani Mendoza <gani.mendoza@gmail.com>
2015-12-22 16:49:14 +0800

    Create oxy.go

---
+++ iris/oxy.go
@@ -0,0 +1,58 @@
+//Reverse Proxy backed by Iris (no need for etcd, Consul, Zookeeper, etc)
+
+package main
+
+import (
+	"flag"
+	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/testutils"
+	"gopkg.in/project-iris/iris-go.v1"
+	"log"
+	"net/http"
+	"time"
+)
+
+var cluster = flag.String("cluster", "", "Iris cluster name")
+
+//https://medium.com/@matryer/the-http-handlerfunc-wrapper-technique-in-golang-c60bf76e6124
+func wrapHandlerFunc(c *iris.Connection, f *forward.Forwarder) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		ip := []byte("ip")
+		var ipaddr string
+		if reply, err := c.Request(*cluster, ip, time.Second*5); err != nil {
+			log.Printf("failed to execute request: %v.", err)
+		} else {
+			ipaddr = string(reply)
+		}
+
+		// let us forward this request to another server
+		req.URL = testutils.ParseURI("http://" + ipaddr + ":63450")
+		f.ServeHTTP(w, req)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := iris.Connect(55555)
+	if err != nil {
+		log.Fatalf("failed to connect to the Iris relay: %v.", err)
+	} else {
+		log.Println("Connected to port 55555")
+	}
+	defer conn.Close()
+
+	// Forwards incoming requests to whatever location URL points to, adds proper forwarding headers
+	fwd, _ := forward.New()
+
+	redirect := wrapHandlerFunc(conn, fwd)
+
+	// that's it! our reverse proxy is ready!
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: redirect,
+	}
+	s.ListenAndServe()
+}
