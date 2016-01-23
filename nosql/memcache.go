9b3f0106cc3d2a01142a46ccdd20874f6321aab1
Isagani Mendoza <gani.mendoza@gmail.com>
2016-01-23 15:59:28 +0800

    Update memcache.go

--- nosql/memcache.go
+++ nosql/memcache.go
@@ -1,19 +1,33 @@
+//Gani Mendoza
+//http://itjumpstart.wordpress.com
+
 package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
 func main() {
-	mc := memcache.New("192.168.0.153:11211")
-	mc.Set(&memcache.Item{Key: "foo", Value: []byte("my value")})
+	mc := memcache.New("192.168.0.153:11211", "192.168.0.153:11212")
+	mc.Set(&memcache.Item{Key: "foo", Value: []byte("my value"),
+		Expiration: int32(time.Now().Add(time.Second * 5).Unix())})
 
 	it, err := mc.Get("foo")
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(it.Value))
+	}
+
+	time.Sleep(time.Second * 6)
+	it, err = mc.Get("foo")
 
 	if err != nil {
 		fmt.Println(err)
