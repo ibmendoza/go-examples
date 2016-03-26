493eda35ca8e639ec0f299a2c197dc8df947f8af
Isagani Mendoza <gani.mendoza@gmail.com>
2016-03-26 14:53:34 +0800

    Create sub2.go

---
+++ nats/sub2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/nats-io/nats"
+	"log"
+	"runtime"
+	"sync/atomic"
+)
+
+var ops uint64 = 0
+
+func main() {
+
+	natsConnection, _ := nats.Connect(nats.DefaultURL)
+	log.Println("Connected to " + nats.DefaultURL)
+
+	// Subscribe to subject
+	log.Printf("Subscribing to subject 'foo'\n")
+	natsConnection.Subscribe("foo", func(msg *nats.Msg) {
+
+		// Handle the message
+		log.Printf("Received message '%s\n", string(msg.Data)+"'")
+		atomic.AddUint64(&ops, 1)
+		log.Println(ops)
+	})
+
+	// Keep the connection alive
+	runtime.Goexit()
+}
