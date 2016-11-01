538a2f23ed958add4e1e0b5fc043fab54a58d7e8
Isagani Mendoza <gani.mendoza@gmail.com>
2016-11-01 15:42:27 +0800

    Update mysql.go

--- sql/mysql.go
+++ sql/mysql.go
@@ -9,8 +9,9 @@ import (
 )
 
 func main() {
+	//http://go-database-sql.org/accessing.html
 	db, err := sql.Open("mysql",
-		"username:password@tcp(127.0.0.1:3306)/pos")
+		"username:password@tcp(127.0.0.1:3306)/databasename")
 	if err != nil {
 		log.Fatal(err)
 	}
