de1d0ea71f18514068aa683091b7eee8432debb0
Isagani Mendoza <gani.mendoza@gmail.com>
2020-03-26 14:54:11 +0800

    Create rockset.go

---
+++ rockset.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	optional "github.com/antihax/optional"
+	"github.com/rockset/rockset-go-client"
+	models "github.com/rockset/rockset-go-client/lib/go"
+)
+
+func main() {
+
+	/* Client function is superseded by NewClient func
+		client := apiclient.Client("apikey",
+			"https://api.rs2.usw2.rockset.com")
+	*/
+
+	client, err := rockset.NewClient(rockset.WithAPIKey("APIkey"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request := &models.ExecuteOpts{
+		Body: optional.NewInterface(models.ExecuteQueryLambdaRequest{
+			Parameters: []models.QueryParameter{
+				{
+					Name:  "country",
+					Type_: "string",
+					Value: "China",
+				},
+			},
+		}),
+	}
+
+	res, _, err := client.QueryLambdas.Execute("commons", "SumStats", 4, request)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("query result: %v\n", res.Results[0])
+
+	fmt.Println("Press Enter Key to exit")
+	fmt.Scanln() // wait for Enter Key
+
+}
